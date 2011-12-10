08be2678c5d9e3af73a4913d2010b39521867942
Cameron <cameronbriar@mail.fresnostate.edu>
2011-12-10 12:17:09 -0800

    Added colors.h
    First add

---
+++ colors.h
@@ -0,0 +1,15 @@
+// File for printing colors
+
+#define RESET   printf("\e[0m")
+#define BLACK   printf("\e[0;30m")
+#define RED     printf("\e[0;31m")
+#define GREEN   printf("\e[0;32m")
+#define YELLOW  printf("\e[0;33m")
+#define BLUE    printf("\e[0;34m")
+#define PURPLE  printf("\e[0;35m")
+#define CYAN    printf("\e[0;36m")
+#define WHITE   printf("\e[0;37m")
+
+
+
+
